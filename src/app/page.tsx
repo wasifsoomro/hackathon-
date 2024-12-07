caf6b2f2647a912b5437d656f55acbe2d5a68799
Wasif Soomro <wasifsoomro761@gmail.com>
2024-12-07 23:10:13 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Header from "./(components)/Header";
+import Footer from "./(components)/Footer";
+
+export default function Home() {
+  return (
+    <div>
+      Wasif Soomro
+
+    <Header />
+    <Footer />
+    </div>
+  );
+}
